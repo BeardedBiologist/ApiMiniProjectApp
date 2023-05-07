51fd7c27e7fbaff8124476e1cba28469d00ffca7
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-07 19:06:31 +0200

    setup post call

---
+++ ApiMiniProject/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        public ILogger _logger;
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogInformation("The person was logged as {Person}", value);
+        }
+
+    }
+}
